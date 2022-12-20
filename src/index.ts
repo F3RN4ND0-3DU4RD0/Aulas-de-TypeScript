b3cd167c0692aa94e15f03c46b6a723c828da120
Fernando <nanduardo20@gmail.com>
2022-12-20 15:02:04 -0300

    Atualizando o meu ts

--- src/index.ts
+++ src/index.ts
@@ -30,3 +30,25 @@ let indefinido: undefined = undefined;
 let vazio: void;
 let qualquerCoisa: any = "Literalmente qualquer coisa";
 let qualquerNumero: any = 40352;
+
+//Trabalhando com Object
+
+//Object - sem previsibilidade
+let produto: object ={
+    name: "Boneco de super-Herói",
+    preço: "99.99",
+    unidades: 10,
+}
+
+//Object tipado - com previsibilidade 
+type ProdutoLoja = {
+    name: string,
+    preço: number,
+    unidades: number,
+}
+
+let meuProduto: ProdutoLoja = {
+    name: "Boneco do Max Steel",
+    preço: 25.90,
+    unidades: 10,
+}
