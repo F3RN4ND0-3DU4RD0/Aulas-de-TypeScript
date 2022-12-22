248a2728ebb659ea8550914a0d704b5988edb4cc
Fernando <nanduardo20@gmail.com>
2022-12-22 11:14:05 -0300

    #Funções

--- build/index.js
+++ build/index.js
@@ -1,52 +1,16 @@
 "use strict";
-/*tipos de variáveis{
-    PRIMITIVOS
-    boolean
-    string
-    number
-
-
-    ESPECIAIS
-    null
-    undefined
-
-    ABRANGENTES
-    void
-    any
-}*/
-//Primitivos
-let Nome = "Fernando";
-let Idade = 20;
-let Altura = 1.85;
-let ligado = true;
-//Especiais
-let nulo = null;
-let indefinido = undefined;
-//Abrangentes
-let vazio;
-let qualquerCoisa = "Literalmente qualquer coisa";
-let qualquerNumero = 40352;
-//Trabalhando com Object
-//Object - sem previsibilidade
-let produto = {
-    name: "Boneco de super-Herói",
-    preço: "99.99",
-    unidades: 10,
-};
-let meuProduto = {
-    name: "Boneco do Max Steel",
-    preço: 25.90,
-    unidades: 10,
-};
-//Trabalhando com Array
-let dados = ["Fernando", "Roberto", "Carlos"];
-let dados2 = ["Adalberto", "Juliano", "Carlos Magno"];
-//Array de múltiplos tipos
-let infos = ["Julio Cesar", 49, true];
-//Tuplas
-let boleto = [20, "dezembro", 2022];
-//Métodos do Javascript servem todos para o Typescript
-dados.pop();
-//Trabalhando com datas
-let meuAniversario = new Date("2002-06-25 13:00");
-console.log(meuAniversario.toString());
+function somar(x, y) {
+    return x + y;
+}
+function mostrarNome(name) {
+    return `Hello ${name}`;
+}
+function CalltoPhone(phone) {
+    return phone;
+}
+let resultado = somar(40, 7);
+console.log(resultado);
+let meuNome = mostrarNome("Fernando");
+console.log(meuNome);
+let liguePara = CalltoPhone("85912344321");
+console.log(liguePara);
--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,22 @@
+//Funções
 
-
-/*tipos de variáveis{
-    PRIMITIVOS
-    boolean
-    string
-    number
-
-
-    ESPECIAIS
-    null
-    undefined
-
-    ABRANGENTES
-    void
-    any
-}*/
-
-//Primitivos
-let Nome: string = "Fernando";
-let Idade: number = 20;
-let Altura = 1.85;
-let ligado: boolean = true;
-
-//Especiais
-let nulo: null = null;
-let indefinido: undefined = undefined;
-
-//Abrangentes
-let vazio: void;
-let qualquerCoisa: any = "Literalmente qualquer coisa";
-let qualquerNumero: any = 40352;
-
-//Trabalhando com Object
-
-//Object - sem previsibilidade
-let produto: object ={
-    name: "Boneco de super-Herói",
-    preço: "99.99",
-    unidades: 10,
+function somar(x: number, y: number): number {
+  return x + y;
 }
 
-//Object tipado - com previsibilidade 
-type ProdutoLoja = {
-    name: string,
-    preço: number,
-    unidades: number,
+function mostrarNome(name: String) {
+  return `Hello ${name}`;
 }
 
-let meuProduto: ProdutoLoja = {
-    name: "Boneco do Max Steel",
-    preço: 25.90,
-    unidades: 10,
+function CalltoPhone(phone: number | string): any {
+  return phone;
 }
 
-//Trabalhando com Array
-let dados: string[] = ["Fernando", "Roberto", "Carlos"];
-let dados2: Array<string> = ["Adalberto","Juliano","Carlos Magno"]
+let resultado: number = somar(40, 7);
+console.log(resultado);
 
-//Array de múltiplos tipos
-let infos: (string | number | boolean)[]=["Julio Cesar", 49, true]
+let meuNome: string = mostrarNome("Fernando");
+console.log(meuNome);
 
-//Tuplas
-let boleto: [number, string, number] = [20,"dezembro",2022];
-
-//Métodos do Javascript servem todos para o Typescript
-dados.pop();
-
-//Trabalhando com datas
-let meuAniversario: Date = new Date("2002-06-25 13:00");
-console.log(meuAniversario.toString());
+let liguePara: number = CalltoPhone(85912344321);
+console.log(liguePara);
