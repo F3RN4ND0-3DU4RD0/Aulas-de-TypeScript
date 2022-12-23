f36031fbb220ea45dc6a76619b120733e7c2f5af
Fernando <nanduardo20@gmail.com>
2022-12-22 21:54:27 -0300

    #Classes

--- build/index.js
+++ build/index.js
@@ -9,5 +9,17 @@ class Character {
         console.log(`${this.name} attack with ${this.strenght} points!`);
     }
 }
+class Magician extends Character {
+    constructor(name, strength, skill, magicPower) {
+        super(name, strength, skill);
+        this.magicPower = magicPower;
+    }
+    iceAttack() {
+        const danoDeGelo = this.magicPower / 2;
+        console.log(`${this.name} attack with iceAttack ${danoDeGelo}`);
+    }
+}
 const p1 = new Character("Zenyem", 10, 98);
+const p2 = new Magician("Mago de Gelo", 15, 110, 150);
 p1.attack();
+p2.iceAttack();
--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,8 @@ obs: quando uma variável tiver (?), significa que ela é opcional.
 
 class Character {
   public name?: string;
-  private strenght: number;
-  protected skill: number;
+  protected strenght: number;
+  private skill: number;
 
   constructor(name: string, strength: number, skill: number) {
     this.name = name;
@@ -25,5 +25,28 @@ class Character {
   }
 }
 
-const p1 = new Character("Zenyem",10,98);
+class Magician extends Character {
+  magicPower: number;
+  constructor(
+    name: string,
+    strength: number,
+    skill: number,
+    magicPower: number
+  ) {
+    super(name, strength, skill);
+
+    this.magicPower = magicPower;
+  }
+
+  //Parte que inclui no código, fora da aula
+  iceAttack(): void {
+    const danoDeGelo = this.magicPower / 2;
+    console.log(`${this.name} attack with iceAttack ${danoDeGelo}`);
+    //Parte que inclui no código, fora da aula
+  }
+}
+
+const p1 = new Character("Zenyem", 10, 98);
+const p2 = new Magician("Mago de Gelo", 15, 110, 150);
 p1.attack();
+p2.iceAttack();
