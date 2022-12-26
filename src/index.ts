58d73c2d53866071315a9952fba152a529dd75e8
Fernando <nanduardo20@gmail.com>
2022-12-26 12:28:59 -0300

    # Última aula sobre componentização

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
 //Decorators
-function apiVersion(version: string){
-    return(target:any) => {
-        Object.assign(target.prototype, {__version: version});
-    };
+//Decorator Class
+function apiVersion(version: string) {
+  return (target: any) => {
+    Object.assign(target.prototype, { __version: version });
+  };
 }
 
-@apiVersion("1.0")
-class Api{}
+//Attribute decorator
+function minLength(length: number) {
+  return (target: any, key: string) => {
+    let _value = target[key];
 
-const api = new Api();
-console.log(api.__version);
+    const getter = () => _value;
+    const setter = (value: string) => {
+      if (value.length < length) {
+        throw new Error(`Tamanho menor que ${length}`);
+      } else {
+        _value = value;
+      }
+    };
+
+  
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+    });
+  };
+}
+
+class Api {
+  @minLength(5)
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+const api = new Api("Carrinho de brinquedo");
+console.log(api.name);
